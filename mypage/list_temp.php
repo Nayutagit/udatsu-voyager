cfda40beb9c5fa67b2de317cef07ec6634d8fcdf
Nayuta <udatsuageteko@gmail.com>
2026-06-02 12:45:34 +0900

    Check users folder existence

--- mypage/list_temp.php
+++ mypage/list_temp.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$path = realpath(__DIR__ . '/../users');
+$path = __DIR__ . '/../users';
 echo "Path: " . $path . "\n";
+echo "Exists: " . (file_exists($path) ? 'YES' : 'NO') . "\n";
 $files = glob($path . '/*.json');
 print_r($files);
