8514f3a3ab359834436548e4d9ff5f8d4d76bb8c
Nayuta <udatsuageteko@gmail.com>
2026-06-02 12:46:11 +0900

    Cleanup temporary diagnostics and restore scripts

--- mypage/list_temp.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$path = __DIR__ . '/../users';
-echo "Path: " . $path . "\n";
-echo "Exists: " . (file_exists($path) ? 'YES' : 'NO') . "\n";
-$files = glob($path . '/*.json');
-print_r($files);
