626828072198610784b8d65705ddf653244fa3b4
Nayuta <udatsuageteko@gmail.com>
2026-06-02 11:46:59 +0900

    debug: add analysis_log.txt to log viewer

--- view_logs_temp.php
+++ view_logs_temp.php
@@ -3,6 +3,7 @@ if (!isset($_GET['key']) || $_GET['key'] !== 'debug123') exit('unauthorized');
 
 $log1 = __DIR__ . '/webhook_debug.txt';
 $log2 = __DIR__ . '/log/run_analysis_log.txt';
+$log3 = __DIR__ . '/log/analysis_log.txt';
 
 echo "<h1>PHP Info</h1>";
 echo "PHP_BINARY: " . PHP_BINARY . "<br>";
@@ -21,3 +22,16 @@ if (file_exists($log2)) {
 } else {
     echo "<p>Not found</p>";
 }
+
+echo "<h1>Analysis Log</h1>";
+if (file_exists($log3)) {
+    $content = file_get_contents($log3);
+    // Limit to last 5000 characters to avoid huge response
+    if (strlen($content) > 5000) {
+        $content = "--- TRUNCATED (showing last 5000 chars) ---\n" . substr($content, -5000);
+    }
+    echo "<pre>" . htmlspecialchars($content) . "</pre>";
+} else {
+    echo "<p>Not found</p>";
+}
+
